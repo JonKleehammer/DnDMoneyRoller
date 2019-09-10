3f554e68ab30edec9fb5766edfdc926bda1c563c
Jon Kleehammer <jjk0102@unt.edu>
2019-09-10 15:02:05 -0500

    Adding file to repository Basic layout of program created

---
+++ MoneyRoller.py
@@ -0,0 +1,21 @@
+# Program by Jonathan Kleehammer 9-10-19
+# Program is designed to simulate DnD 5e treasure hoards from pages 137-138 of the DMG
+# Only simulates the gold because the other values are generated/picked elsewhere
+# also takes in a number of players so that the money is evenly divisible
+
+
+# takes in a dice string (3d6, 4d8, 2d4, etc) and returns an integer for what was rolled
+def diceroller(diceString):
+    pass
+
+def convertToGold(coinType, amount):
+    pass
+
+# take in user input to choice the CR of the hoard
+# 0 : 0-4
+# 1 : 5-10
+# 2 : 11-16
+# 3 : 17+
+choice = 0
+
+# actually rolling the money and gems
